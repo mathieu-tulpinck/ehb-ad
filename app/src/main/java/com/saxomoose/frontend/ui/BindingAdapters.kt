3a6cc65c149a16d5be0679e40c22e7afb870cf88
saxomoose <mathieu.developer@protonmail.com>
2022-08-02 15:31:55 +0200

    TransactionFragment.

--- app/src/main/java/com/saxomoose/frontend/ui/BindingAdapters.kt
+++ app/src/main/java/com/saxomoose/frontend/ui/BindingAdapters.kt
@@ -1,14 +1,9 @@
 package com.saxomoose.frontend.ui
 
-import android.annotation.SuppressLint
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.saxomoose.frontend.models.Event
 import com.saxomoose.frontend.models.TransactionItem
-import com.saxomoose.frontend.ui.events.EventAdapter
-import java.text.SimpleDateFormat
-import java.util.*
 
 @BindingAdapter("transactionItems")
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<TransactionItem>?) {
